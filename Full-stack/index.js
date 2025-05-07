d671a875ba45f86a325ff15922b8dc8223490256
Chesta bari <chestabari@gmail.com>
2025-05-07 14:00:54 +0530

    Added Full-stack project

---
+++ Full-stack/index.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import dotenv from "dotenv";
+import cors from "cors";
+import db from './utils/db.js';
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+    cors({
+        origin: process.env.BASE_URL,
+        credentials: true,
+        methods: ['GET', 'POST', 'DELETE','OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    })
+);
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/chesta", (req, res) => {
+    res.send("Chesta!");
+  });
+
+app.get("/uday", (req, res) => {
+    res.send("Uday!");
+  });
+
+// connect to db
+db();
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
