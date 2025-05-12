c5231efd2c3a593adc9ad8b86984ac1a7ced430d
Chesta bari <chestabari@gmail.com>
2025-05-12 16:23:53 +0530

    All updates

--- Full-stack/model/User.model.js
+++ Full-stack/model/User.model.js
@@ -1,19 +1,17 @@
 import mongoose from "mongoose";
-import bcryptjs from "bcryptjs";
 
-const userSchema = new mongoose.Schema(
-    {
+const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     password: String,
     role: {
         type: String,
         enum: ["user", "admin"],
-        default: "user"
+        default: "user",
     },
     isVerified: {
         type: Boolean,
-        default: false
+        default: false,
     },
     verificationToken: {
         type: String,
@@ -21,22 +19,15 @@ const userSchema = new mongoose.Schema(
     resetPasswordToken: {
         type: String,
     },
-    resetPasswordExpires: {
+    resetPasswordExpries: {
         type: Date,
     },
-    }, 
+ }, 
     {
-    timestamps: true,
+        timestamps: true,
     }
 );
 
-userSchema.pre("save", async function(next) {
-    if(this.isModified("password")){
-        this.password = await bcryptjs.hash(this.password, 10);
-    }
-    next();
-})
+const User = mongoose.model("User", userSchema)
 
-const User = mongoose.model("User", userSchema);
-
-export default User;
+export default User
